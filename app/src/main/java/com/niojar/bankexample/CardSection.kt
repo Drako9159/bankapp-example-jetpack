285accb95b2ca304f070bed051b95eae593e7387
drako9159 <chochito9159@gmail.com>
2025-01-06 15:28:03 -0600

    feat(): configure section finance

--- app/src/main/java/com/niojar/bankexample/CardSection.kt
+++ app/src/main/java/com/niojar/bankexample/CardSection.kt
@@ -129,7 +129,5 @@ fun CardItem(index: Int) {
             )
         }
     }
-
-
 }
 
--- app/src/main/java/com/niojar/bankexample/MainActivity.kt
+++ app/src/main/java/com/niojar/bankexample/MainActivity.kt
@@ -85,7 +85,7 @@ fun HomeScreen() {
             WalletSection()
             CardSection()
             Spacer(modifier = Modifier.height(16.dp))
-            //FinanceSection()
+            FinanceSection()
             //CurreniesSection()
         }
 
