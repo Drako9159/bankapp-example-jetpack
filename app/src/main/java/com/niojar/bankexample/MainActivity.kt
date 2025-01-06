178db0a1e39e0fc5a4560e338fc28487818b3945
drako9159 <chochito9159@gmail.com>
2025-01-06 13:41:56 -0600

    feat(icons): add ic back

--- app/src/main/java/com/niojar/bankexample/MainActivity.kt
+++ app/src/main/java/com/niojar/bankexample/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.niojar.bankexample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.niojar.bankexample.ui.theme.BankexampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +29,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BankexampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                SetBarColor(color = MaterialTheme.colorScheme.background)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
-                }
+                }*/
             }
         }
     }
@@ -43,5 +61,34 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     BankexampleTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+private fun SetBarColor(color: Color) {
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = color
+        )
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Preview
+@Composable
+fun HomeScreen() {
+    Scaffold(bottomBar = {
+        BottomNavigationBar()
+    }) { padding ->
+        Column(modifier = Modifier.padding(padding)) {
+            WalletSection()
+            CardSection()
+            Spacer(modifier = Modifier.height(16.dp))
+            FinanceSection()
+            CurreniesSection()
+        }
+
+
     }
 }
